6a9aa5568c5447d3854d3ea4fd08cb9ca3ca0a66
bannplayer <51109507+bannplayer@users.noreply.github.com>
2019-10-02 15:13:47 +0900

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -67,7 +67,7 @@ module.exports = {
     rinkeby: {
        provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/infura_project_id`),
        network_id: 4,       // Ropsten's id
-       gas: 6500000,        // Ropsten has a lower block limit than mainnet
+       gas: 6700000,        // Ropsten has a lower block limit than mainnet
       // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
       // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
        skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
