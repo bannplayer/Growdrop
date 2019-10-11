487dff2f7de5d814dd90fae3d15eff2ec8476b84
bannplayer <51109507+bannplayer@users.noreply.github.com>
2019-10-11 17:21:47 +0900

    Update EIP20Interface.sol

--- contracts/EIP20Interface.sol
+++ contracts/EIP20Interface.sol
@@ -14,6 +14,8 @@ contract EIP20Interface {
 
     function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
 
+    function decimals() public view returns(uint digits);
+
     event Transfer(address indexed from, address indexed to, uint256 value);
 
     event Approval(address indexed owner, address indexed spender, uint256 value);
