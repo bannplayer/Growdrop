58ccbcc728d19696be07b5c008fb5b94abef79c1
bannplayer <51109507+bannplayer@users.noreply.github.com>
2019-11-03 01:55:37 +0900

    Add files via upload

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,4 +1,5 @@
 const Growdrop = artifacts.require("Growdrop");
+const Growdrop_Kovan = artifacts.require("Growdrop_kovan");
 const Tokenswap = artifacts.require("Tokenswap");
 const DonateToken = artifacts.require("DonateToken");
 const GrowdropCall = artifacts.require("GrowdropCall");
@@ -15,28 +16,45 @@ module.exports = function(deployer, network) {
       kyberdai="0xC4375B7De8af5a38a93548eb8453a498222C4fF2";
       dai="0xbF7A7169562078c96f0eC1A8aFD6aE50f12e5A99";
       kyberminimum="1000000000";
-  } else if (network == "live") {
+
+      deployer.deploy(Growdrop_Kovan)
+      .then(function() {
+        return deployer.deploy(GrowdropCall, Growdrop_Kovan.address);
+      }).then(function() {
+        return deployer.deploy(
+          Tokenswap, 
+          Growdrop_Kovan.address,
+          uniswapfactory, 
+          kybernetworkproxy, 
+          dai,
+          kyberdai,
+          kyberminimum
+        );
+      }).then(function() {
+        return deployer.deploy(DonateToken, Growdrop_Kovan.address);
+      })
+  } else if (network == "mainnet") {
       uniswapfactory="0xc0a47dFe034B400B47bDaD5FecDa2621de6c4d95"
       kybernetworkproxy="0x818E6FECD516Ecc3849DAf6845e3EC868087B755";
       dai="0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359";
       kyberdai="0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359";
       kyberminimum="1000000000000000";
-  }
 
-  deployer.deploy(Growdrop)
-  .then(function() {
-    return deployer.deploy(GrowdropCall, Growdrop.address);
-  }).then(function() {
-    return deployer.deploy(
-      Tokenswap, 
-      Growdrop.address,
-      uniswapfactory, 
-      kybernetworkproxy, 
-      dai,
-      kyberdai,
-      kyberminimum
-    );
-  }).then(function() {
-    return deployer.deploy(DonateToken, Growdrop.address);
-  })
+      deployer.deploy(Growdrop)
+      .then(function() {
+        return deployer.deploy(GrowdropCall, Growdrop.address);
+      }).then(function() {
+        return deployer.deploy(
+          Tokenswap, 
+          Growdrop.address,
+          uniswapfactory, 
+          kybernetworkproxy, 
+          dai,
+          kyberdai,
+          kyberminimum
+        );
+      }).then(function() {
+        return deployer.deploy(DonateToken, Growdrop.address);
+      })
+  }
 }
